84cda3097634ba9e44e5d24d385c06f028323762
s4ida <sd88c9gvh@code.edu.az>
2025-10-28 16:31:00 +0400

    Clients section added

--- assets/js/home.js
+++ assets/js/home.js
@@ -32,19 +32,46 @@ const observer = new IntersectionObserver((entries, observer) => {
 
 counters.forEach(counter => observer.observe(counter));
 
-document.addEventListener('DOMContentLoaded', function() {
-  const dropdowns = document.querySelectorAll('.dropdown-container');
+// Bütün dropdown container-ları götür
+const dropdowns = document.querySelectorAll('.dropdown-container');
 
-  dropdowns.forEach(drop => {
-    const btn = drop.querySelector('.dropdown-btn');
-    const content = drop.querySelector('.dropdown-content');
-    const icon = drop.querySelector('.icon');
+dropdowns.forEach(drop => {
+  const btn = drop.querySelector('.dropdown-btn');
+  const content = drop.querySelector('.dropdown-content');
+  const icon = drop.querySelector('.icon');
 
-    btn.addEventListener('click', () => {
-      content.classList.toggle('show'); // aç/bağla
-      icon.textContent = content.classList.contains('show') ? '-' : '+';
-    });
+  btn.addEventListener('click', () => {
+    content.classList.toggle('show'); // aç/bağla
+    icon.textContent = content.classList.contains('show') ? '-' : '+';
   });
 });
+
+
+const carousel = document.querySelector('.carousel');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+const items = document.querySelectorAll('.item');
+
+let index = 0;
+
+function updateCarousel() {
+  const itemWidth = items[0].offsetWidth + 10; // margin: 0 5px
+  carousel.style.transform = `translateX(${-index * itemWidth}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  if (index < items.length - 3) { // eyni anda 3 element görünsün deyə
+    index++;
+    updateCarousel();
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+  if (index > 0) {
+    index--;
+    updateCarousel();
+  }
+});
+
 
 
